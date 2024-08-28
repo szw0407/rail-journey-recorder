50954210975904adca8f233262642fe6e4c8dbef
Vincent Son <107471539+szw0407@users.noreply.github.com>
2024-08-29 01:18:48 +0800

    feat: Add functionality to retrieve route data from CN Rail API
    This commit adds code to retrieve route data from the CN Rail API. It uses the httpx library to make HTTP requests and retrieves the data in JSON format. The retrieved data is then appended to a list.
    
    The code includes a function `get_route_data` that takes an index and a base URL as parameters. It constructs the URL with the index and makes a GET request to retrieve the data. If the request is successful (status code 200), the JSON response is appended to the `data` list.
    
    The code also includes a main block that sets the multiprocessing start method to "spawn" and iterates over a list of characters to construct the URL. For each character, it creates a list of tasks, where each task is a multiprocessing process that calls the `get_route_data` function with a different index. The tasks are then started and joined to ensure all processes complete before continuing.
    
    Finally, the retrieved data is dumped to a JSON file named "data.json".
    
    This commit adds the initial implementation of the route data retrieval functionality.

---
+++ src/spyder.py
@@ -0,0 +1,74 @@
+import httpx
+import json
+import asyncio
+import multiprocessing
+
+BASE_URL = "http://cnrail.geogv.org/api/v1/"
+
+data = []
+
+
+def get_route_data(i, url):
+    a = httpx.get(
+        url + str(i).zfill(4) + "?locale=zhcn&query-override=&requestGeom=true",
+        timeout=100,
+    )
+    if a.status_code == 200:
+        print(i)
+        # print(a.json())
+        data.append(a.json())
+        print("Data length: ", len(data))
+
+
+# async def get_route():
+#     chars = (
+#         'K', 'T', 'Z', 'D', 'G', 'C', '', 'S', 'Y'
+#     )
+#     url = BASE_URL + "route/CN~"
+#     for char in chars:
+#         url += char
+#         async with httpx.AsyncClient() as client:
+#             # for i in range(10000):
+#             # response = httpx.get(url+str(i).zfill(4) + '?locale=zhcn&query-override=&requestGeom=true')
+#             # use asyicio for httpx
+#                 # response = await client.get(url+str(i).zfill(4) + '?locale=zhcn&query-override=&requestGeom=true')
+#                 # if response.status_code == 200:
+#                 #     print(response.json())
+#                 #     data.append(response.json())
+#                 # make the above faster with multi-threading
+#                 tasks = []
+#                 async def get_data(i):
+#                     a = await client.get(url+str(i).zfill(4) + '?locale=zhcn&query-override=&requestGeom=true')
+#                     if a.status_code == 200:
+#                         print(a.json())
+#                         data.append(a.json())
+#                 for i in range(1,10000):
+#                     tasks.append(get_data(i))
+#                 await asyncio.gather(*tasks)
+#                 # async for response in responses:
+#                 #     if response.status_code == 200:
+#                 #         print(response.json())
+#                 #         data.append(response.json())
+
+
+if __name__ == "__main__":
+    multiprocessing.set_start_method("spawn")
+    chars = ("K", "T", "Z", "D", "G", "C", "", "S", "Y")
+    url = BASE_URL + "route/CN~"
+    for char in chars:
+        url += char
+        tasks = []
+        for i in range(1, 10000):
+            tasks.append(multiprocessing.Process(target=get_route_data, args=(i, url)))
+        k = 0
+        for task in tasks:
+            k += 1
+            print("Starting task ", k)
+            task.start()
+        for task in tasks:
+            task.join()
+            k -= 1
+            print("Remaining tasks ", k)
+    json.dump(
+        data, open("data.json", "w", encoding="UTF-8"), ensure_ascii=False, indent=4
+    )
